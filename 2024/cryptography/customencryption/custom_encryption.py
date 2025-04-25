9f4464a9335959e790418f6b9cdd5b75432400da
syafiqparadisam <syafiqparadisam@gmail.com>
2025-04-25 20:46:49 +0700

    ganti os

--- 2024/cryptography/customencryption/custom_encryption.py
+++ 2024/cryptography/customencryption/custom_encryption.py
@@ -13,10 +13,9 @@ def encrypt(plaintext, key):
     return cipher
 
 def decrypt(cipher,key):
-    plain_text = ""
-    print(key)
+    plain_text = []
     for char in cipher:
-        plain_text += chr(char // (key * 311))
+        plain_text.append(chr(char // (key * 311)))
 
     return plain_text
 
@@ -48,20 +47,18 @@ def test(plain_text, text_key):
     if not is_prime(p) and not is_prime(g):
         print("Enter prime numbers")
         return
-    a = randint(p-10, p)
-    b = randint(g-10, g)
+    # a = randint(p-10, p)
+    # b = randint(g-10, g)
+    a = 89
+    b = 27
     print(f"a = {a}")
     print(f"b = {b}")
     u = generator(g, a, p)
     v = generator(g, b, p)
 
-    print(f"u = {u}")
-    print(f"v = {v}")
     key = generator(v, a, p)
-    print(f"key = {key}")
 
     b_key = generator(u, b, p)
-    print(f"b_key = {b_key}")
     shared_key = None
     print(f"shared_key = {shared_key}")
     if key == b_key:
@@ -72,13 +69,16 @@ def test(plain_text, text_key):
     semi_cipher = dynamic_xor_encrypt(plain_text, text_key)
     
     print(f"semi_cipher = {semi_cipher}")
-    cipher = encrypt(semi_cipher, shared_key)
+    # cipher = encrypt(semi_cipher, shared_key)
+    cipher = [33588, 276168, 261240, 302292, 343344, 328416, 242580, 85836, 82104, 156744, 0, 309756, 78372, 18660, 253776, 0, 82104, 320952, 3732, 231384, 89568, 100764, 22392, 22392, 63444, 22392, 97032, 190332, 119424, 182868, 97032, 26124, 44784, 63444]
 
-    plaintext = decrypt(cipher, shared_key)
+    semi_plain = decrypt(cipher, shared_key)
+    plaintext = dynamic_xor_encrypt(semi_plain, text_key)
     print(f'cipher is: {cipher}')
     print(f'plaintext is: {plaintext}')
 
 
 if __name__ == "__main__":
-    message = sys.argv[1]
+    # message = sys.argv[
+    message = ""
     test(message, "trudeau")
